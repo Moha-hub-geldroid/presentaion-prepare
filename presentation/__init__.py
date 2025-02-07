659f59db7fd0a5e27f4a26164e13fa59fc820e5b
mohammad <81656341+Moha-hub-geldroid@users.noreply.github.com>
2025-02-07 16:59:57 +0300

    Update __init__.py

--- presentation/__init__.py
+++ presentation/__init__.py
@@ -5,7 +5,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "ec29e9c6c1116fe18003de25ffa6805200e8711d42ab0cadce7e85b52d07b8c7"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ju_users_record.db"  
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ju_users.db"  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 
