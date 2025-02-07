cb3d2f6376ca83d2a7c6b6c17e5d1fea21bf8f1e
mohammad <81656341+Moha-hub-geldroid@users.noreply.github.com>
2025-02-07 17:41:46 +0300

    Update routes.py

--- presentation/routes.py
+++ presentation/routes.py
@@ -10,17 +10,6 @@ from werkzeug.exceptions import NotFound
 @app.route("/")
 @app.route("/home",methods=["GET"])
 def home():
-    hashed_password = generate_password_hash("mohammadzinc", method="pbkdf2:sha256")
-    with app.app_context():
-        db.create_all()
-        new_user = User(
-        first_name="Mohammad",
-        last_name="Mhaidat",
-        email="mohammadzinc@ju.edu.jo",
-        password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-
     return render_template("home.html",title="Home Page")
 
 @app.route('/login',methods=["GET","POST"])
